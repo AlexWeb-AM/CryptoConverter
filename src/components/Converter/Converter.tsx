2119d0c1d880e273278bdad1ebe669473420d11c
Alex <balyanalex272@gmail.com>
2024-08-31 22:13:25 +0400

    building

---
+++ src/components/Converter/Converter.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { FaReact } from "react-icons/fa";
+
+interface Crypto {
+  id: string;
+  symbol: string;
+  name: string;
+  priceUsd: string;
+}
+
+type CryptoData = Crypto[];
+
+const Converter = () => {
+  const [cryptoData, setCryptoData] = useState<CryptoData | null>(null);
+  const [amount, setAmount] = useState<number>(1);
+  const [fromCrypto, setFromCrypto] = useState<string>('');
+  const [toCrypto, setToCrypto] = useState<string>('');
+  const [convertedAmount, setConvertedAmount] = useState<number | null>(null);
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then((response) => response.json())
+      .then((data) => setCryptoData(data.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(parseFloat(e.target.value));
+  };
+
+  const handleFromCryptoChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFromCrypto(e.target.value);
+  };
+
+  const handleToCryptoChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setToCrypto(e.target.value);
+  };
+
+  useEffect(() => {
+    if (cryptoData && fromCrypto && toCrypto) {
+      const fromPrice = cryptoData.find(crypto => crypto.id === fromCrypto)?.priceUsd;
+      const toPrice = cryptoData.find(crypto => crypto.id === toCrypto)?.priceUsd;
+      
+      if (fromPrice && toPrice) {
+        const amountInUsd = amount * parseFloat(fromPrice);
+        const converted = amountInUsd / parseFloat(toPrice);
+        setConvertedAmount(converted);
+      }
+    }
+  }, [amount, fromCrypto, toCrypto, cryptoData]);
+
+  if (!cryptoData) {
+    return <div>Loading...</div>;
+  }
+
+  
+
+  return (
+    <div className="converter_div">
+      <div className="converter">
+        <header className="converter_header">
+          <h3>Currency Converter</h3>
+          <FaReact className="icon-react" />
+        </header>
+        <main className="convert_main">
+          <div>
+            <input
+              type="number"
+              value={amount}
+              onChange={handleAmountChange}
+              step="any"
+            />
+            <select value={fromCrypto} onChange={handleFromCryptoChange}>
+              {cryptoData.map((crypto) => (
+                <option key={crypto.id} value={crypto.id}>
+                  {crypto.name} ({crypto.symbol})
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex">
+            <input
+              type="number"
+              value={convertedAmount ?? ''}
+              readOnly
+            />
+            <select value={toCrypto} onChange={handleToCryptoChange}>
+              {cryptoData.map((crypto) => (
+                <option key={crypto.id} value={crypto.id}>
+                  {crypto.name} ({crypto.symbol})
+                </option>
+              ))}
+            </select>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Converter;
